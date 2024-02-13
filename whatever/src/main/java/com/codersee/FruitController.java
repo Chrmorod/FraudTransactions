4ecdba1b2167e9ded1b0bc5dabddd8c66c4ae213
Chmoro <christianmr92@gmail.com>
2024-02-13 22:30:27 +0100

    prueba

---
+++ whatever/src/main/java/com/codersee/FruitController.java
@@ -0,0 +1,37 @@
+package com.codersee;
+
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Status;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import java.util.List;
+
+import static io.micronaut.http.HttpStatus.CREATED;
+
+@Controller("/fruits")
+@ExecuteOn(TaskExecutors.BLOCKING)
+class FruitController {
+
+    private final FruitRepository fruitService;
+
+    FruitController(FruitRepository fruitService) {
+        this.fruitService = fruitService;
+    }
+
+    @Get
+    List<Fruit> list() {
+        return fruitService.list();
+    }
+
+    @Post
+    @Status(CREATED)
+    void save(@NonNull @NotNull @Valid Fruit fruit) {
+        fruitService.save(fruit);
+    }
+}
---
+++ whatever/src/main/java/com/codersee/FruitRepository.java
@@ -0,0 +1,15 @@
+package com.codersee;
+
+import io.micronaut.core.annotation.NonNull;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import java.util.List;
+
+public interface FruitRepository {
+
+    @NonNull
+    List<Fruit> list();
+
+    void save(@NonNull @NotNull @Valid Fruit fruit);
+}
