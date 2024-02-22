1d7de9af357f14ed70c931b4e4db0b3f8e99d780
Chmoro <christianmr92@gmail.com>
2024-02-23 00:18:30 +0100

    Update App Generator Transactions

--- App_Generator_Transactions/src/Components/GenerateData/GenerateData.js
+++ App_Generator_Transactions/src/Components/GenerateData/GenerateData.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Home } from "../Home/Home";
 import './GenerateData.css';
+class HomeView extends React.Component{
+    render(){
+      return (
+        <Home/>
+      )
+    }
+}
 export class GenerateData extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             generate: "",
-            dataForm: []
+            dataForm: [],
+            home: false
         };
     }
     setGenerate = (generate) => {
@@ -19,14 +28,46 @@ export class GenerateData extends React.Component {
             const response = await fetch(TRX_API_URL, {
                 method: 'GET',
                 headers: {
-                    'Content-Type': 'application/json',
-                },
+                    'Content-Type': 'application/json'
+                }
             });
     
             if (response.ok) {
                 const data = await response.json();
-                this.setState({ dataForm: data });
                 console.log('Datos extraidos exitosamente');
+                 // Metodo Post con los datos obtenidos
+                 await Promise.all(data.map(async (elemento) => {
+                    try {
+                        const postData = {
+                            step: elemento.step,
+                            type: elemento.type,
+                            amount: elemento.amount,
+                            nameOrig: elemento.nameOrig,
+                            oldBalanceOrg: elemento.oldBalanceOrg,
+                            newBalanceOrig: elemento.newBalanceOrig,
+                            nameDest: elemento.nameDest,
+                            oldBalanceDest: elemento.oldBalanceDest,
+                            newBalanceDest: elemento.newBalanceDest,
+                            isFraud: elemento.isFraud
+                        };
+                        const POST_URL = 'http://localhost:8080/bank/transactions'; // Cambia esto a la dirección real
+                        const postResponse = await fetch(POST_URL, {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(postData)
+                        });
+
+                        if (postResponse.ok) {
+                            console.log('Solicitud POST exitosa:', postData);
+                        } else {
+                            console.error('Error en la solicitud POST:', postData);
+                        }
+                    } catch (error) {
+                        console.error('Error en la solicitud POST:', error);
+                    }
+                }));
             } else {
                 console.error('Error al intentar extraer los datos');
             }
@@ -34,33 +75,25 @@ export class GenerateData extends React.Component {
             console.error('Error de red:', error);
         }
     };
+    setgoback = (setgoback) => {this.setState({home:setgoback})}
     render(){
         const { dataForm } = this.state;
         return(
-            <>
-            <h1>Send Transactions</h1>
-            <form>
-                <input className= "input-url" type="text" autoFocus required value={this.state.generate} onChange={(e) => this.setGenerate(e.target.value)} />
-                <button className="btn-generate" type="submit" onClick={this.handleGenerate}>Generate</button>
-                <ul>
-                    {dataForm.map((elemento, index) => (
-                        <li key={index}>
-                            <p>Step: {elemento.step}</p>
-                            <p>Type: {elemento.type}</p>
-                            <p>Amount: {elemento.amount}</p>
-                            <p>NameOrig: {elemento.nameOrig}</p>
-                            <p>Old Balance Orig: {elemento.oldbalanceOrg}</p>
-                            <p>New Balance Orig: {elemento.newbalanceOrig}</p>
-                            <p>NameDest: {elemento.nameDest}</p>
-                            <p>Old Balance Dest: {elemento.oldbalanceDest}</p>
-                            <p>New Balance Dest: {elemento.newbalanceDest}</p>
-                            <p>Is Fraud: {elemento.isFraud}</p>
-                            <p>Is Flagged Fraud: {elemento.isFlaggedFraud}</p>
-                        </li>
-                    ))}
-                </ul>
-            </form>
-            </>
-        )
-    }
+                <>
+                {this.state.home ? (
+                    <HomeView/>
+                ):(
+                    <>
+                    <h1>Send Transactions</h1>
+                    <button className = "btn-back" onClick={this.setgoback}>Back</button>
+                    <form>
+                        <input className= "input-url" type="text" autoFocus required value={this.state.generate} onChange={(e) => this.setGenerate(e.target.value)} />
+                        <button className="btn-general" type="submit" onClick={this.handleGenerate}>Generate</button>
+                    </form>
+                    </>
+                )
+                }
+                </>
+            )
+        }
 }
--- App_Generator_Transactions/src/Components/SendTransaction/SendTransaction.js
+++ App_Generator_Transactions/src/Components/SendTransaction/SendTransaction.js
@@ -1,22 +1,29 @@
 import React from "react";
-import Datetime from '../DateTime';
+import {Home} from '../Home/Home'
 import './SendTransaction.css';
 const TRX_API_URL = "http://localhost:8080/transactions";
+class HomeView extends React.Component{
+  render(){
+    return (
+      <Home/>
+    )
+  }
+}
 export class SendTransaction extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            type: "",
-            amount: "",
+            typeOptions: ["CASH-IN", "CASH-OUT", "DEBIT", "PAYMENT", "TRANSFER"],
+            type: "CASH-IN",
+            amount: 0.0,
             sender: "",
-            oldbalancesender: "",
-            newbalancesender: "",
+            oldbalancesender: 0.0,
+            newbalancesender: 0.0,
             recipient: "",
-            oldbalancerecipient: "",
-            newbalancerecipient: ""
+            oldbalancerecipient: 0.0,
+            newbalancerecipient: 0.0
         };
     }
-
     setSender = (sender) => { this.setState({ sender: sender }); }
     setOldBalanceSender = (oldbalancesender) => { this.setState({ oldbalancesender: oldbalancesender }); }
     setNewBalanceSender = (newbalancesender) => { this.setState({ newbalancesender: newbalancesender }); }
@@ -25,7 +32,7 @@ export class SendTransaction extends React.Component {
     setRecipient = (recipient) => { this.setState({ recipient: recipient }); }
     setOldBalanceRecipient = (oldbalancerecipient) => { this.setState({ oldbalancerecipient: oldbalancerecipient }); }
     setNewBalanceRecipient = (newbalancerecipient) => { this.setState({ newbalancerecipient: newbalancerecipient }); }
-
+    setgoback = (setgoback) => {this.setState({home:setgoback})}
     handleSubmit = async (event) => {
       event.preventDefault();
   
@@ -41,11 +48,9 @@ export class SendTransaction extends React.Component {
                 "nameDest": this.state.recipient,
                 "oldbalanceDest": this.state.oldbalancerecipient,
                 "newbalanceDest": this.state.newbalancerecipient,
-                "isFraud": 0,
-                "isFlaggedFraud": 0
+                "isFraud": 0
               }
           };
-  
           const response = await fetch(TRX_API_URL, {
               method: 'POST',
               headers: {
@@ -67,57 +72,55 @@ export class SendTransaction extends React.Component {
     render(){
         return(
           <>
-            <section>
-              <div class="title-datetime-container">
-                <h1>Create Transactions</h1>
-                <Datetime />
-              </div>
-            </section>
-            <section>
-              <form onSubmit={this.handleSubmit} class="section-body">
-                <div class="card-container sender-card">
-                  <h2>Sender</h2>
-                  <label>Sender</label>
-                  <input type="text" autoFocus required value={this.state.sender} onChange={(e) => this.setSender(e.target.value)} />
-                  <label>Old Balance Sender</label>
-                  <input type="text" autoFocus required value={this.state.oldbalancesender} onChange={(e) => this.setOldBalanceSender(e.target.value)} />
-                  <label>New Balance Sender</label>
-                  <input type="text" autoFocus required value={this.state.newbalancesender} onChange={(e) => this.setNewBalanceSender(e.target.value)} />
+          {this.state.home ? (
+              <HomeView/>
+          ):(
+            <>
+              <section>
+                <div class="title-datetime-container">
+                  <h1>Create Transactions</h1>
+                  <button className="btn-back" onClick={this.setgoback}>Back</button>
                 </div>
-                <div class="arrow-right"></div>
-                <div class="card-container amount-type-card">
-                  <div class="arrow-container">
-                    <div class="arrow"></div>
+              </section>
+              <section>
+                <form onSubmit={this.handleSubmit} class="section-body">
+                <table>
+                  <tr>
+                      <th>Step</th>
+                      <th>NameOrig</th>
+                      <th>OldBalanceOrig</th>
+                      <th>NewBalanceOrig</th>
+                      <th>Type</th>
+                      <th>Amount</th>
+                      <th>NameDest</th>
+                      <th>OldBalanceDest</th>
+                      <th>NewBalanceDest</th>
+                  </tr>
+                  <tr>
+                      <td>1</td>
+                      <td><input type="text" autoFocus required value={this.state.sender} onChange={(e) => this.setSender(e.target.value)} /></td>
+                      <td><input type="number" step="any" autoFocus required value={this.state.oldbalancesender} onChange={(e) => this.setOldBalanceSender(e.target.value)} name="oldBalanceSender"/></td>
+                      <td><input type="number" step="any" autoFocus required value={this.state.newbalancesender} onChange={(e) => this.setNewBalanceSender(e.target.value)} name ="newBalanceSender" /></td>                      
+                      <td width='200px'>
+                        <select id="type-transac" name="type" type="text" autoFocus required value={this.state.type} onChange={(e) => this.setType(e.target.value)} >
+                          {this.state.typeOptions.map(option => (<option value={option}>{option}</option>))}
+                        </select>
+                      </td>
+                      <td><input type="number" step="any" autoFocus required value={this.state.amount} onChange={(e) => this.setAmount(e.target.value)} name="amount"/></td>
+                      <td><input type="text" autoFocus required value={this.state.recipient} onChange={(e) => this.setRecipient(e.target.value)} /></td>
+                      <td><input type="number" step="any" autoFocus required value={this.state.oldbalancerecipient} onChange={(e) => this.setOldBalanceRecipient(e.target.value)} name="oldBalanceRecipient"/></td>
+                      <td><input type="number" step="any" autoFocus required value={this.state.newbalancerecipient} onChange={(e) => this.setNewBalanceRecipient(e.target.value)} name="newBalanceRecipient"/></td>
+                  </tr>
+              </table>
+                </form>
+              </section>
+              <section>
+                  <div className="section-btn">
+                      <button className="btn-general" type="submit" onClick={this.handleSubmit}>Register Transaction</button>
                   </div>
-                  <h2>Amount & Type</h2>
-                  <label>Amount</label>
-                  <input type="text" autoFocus required value={this.state.amount} onChange={(e) => this.setAmount(e.target.value)} />
-                  <label for="type-transac">Type</label>
-                  <select id="type-transac" name="type" type="text" autoFocus required value={this.state.type} onChange={(e) => this.setType(e.target.value)} >
-                    <option value="cash-in">CASH-IN</option>
-                    <option value="cash-out">CASH-OUT</option>
-                    <option value="debit">DEBIT</option>
-                    <option value="payment">PAYMENT</option>
-                    <option value="transfer">TRANSFER</option>
-                  </select>
-                </div>
-                <div class="arrow-right"></div>
-                <div class="card-container recipient-card">
-                  <h2>Recipient</h2>
-                  <label>Recipient</label>
-                  <input type="text" autoFocus required value={this.state.recipient} onChange={(e) => this.setRecipient(e.target.value)} />
-                  <label>Old Balance Recipient</label>
-                  <input type="text" autoFocus required value={this.state.oldbalancerecipient} onChange={(e) => this.setOldBalanceRecipient(e.target.value)} />
-                  <label>New Balance Recipient</label>
-                  <input type="text" autoFocus required value={this.state.newbalancerecipient} onChange={(e) => this.setNewBalanceRecipient(e.target.value)} />
-                </div>
-              </form>
-            </section>
-            <section>
-                <div className="section-btn">
-                    <button className="btn-general" type="submit" onClick={this.handleSubmit}>Register Transaction</button>
-                </div>
-            </section>
+              </section>
+            </>
+          )}
           </>
         )
     }
